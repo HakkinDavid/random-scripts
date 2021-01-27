56868981b4ba25f133a3aa3260a05238eddf74e6
HakkinDavid <desr11@hotmail.com>
2021-01-27 12:15:51 -0800

    Update a.js

--- a.js
+++ a.js
@@ -3,6 +3,10 @@ let file = fs.readFileSync('input.txt', 'utf-8', console.error).split('\n');
 for (i=0; file.length > i; i++) {
   let index = file[i].search(': ') + 2;
   file[i] = file[i].substring(index);
+  if (!file[i].startsWith("https://youtu.be/")) {
+    file.splice(i, 1);
+    i--
+  }
 }
 
 fs.writeFile('output.txt', file.join("\n"), console.error)
