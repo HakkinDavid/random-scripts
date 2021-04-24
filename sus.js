8751dd750745ba13bd54976dfb31c756708df7f8
HakkinDavid <desr11@hotmail.com>
2021-04-24 02:17:03 -0600

    AMOGUS

--- sus.js
+++ sus.js
@@ -6,24 +6,38 @@ const rl = readline.createInterface({
 });
 const Sound = require('node-aplay');
 let isAMOGUSREFERENCEed = fs.existsSync('./amogus.wav');
-let sussound;
-if (isAMOGUSREFERENCEed) {
-  sussound = new Sound('./amogus.wav');
-}
-console.log("Input your crewmates list to find the imposter.\n");
+console.log("Input your text to find the imposter.\n");
 let sus;
-let find = /[^ඞ]{1}/gi;
+let find = /ඞ{1}/g;
 let imposter;
 rl.on('line', (input) => {
   sus = input;
   imposter = find.exec(sus);
+  let susARRAY = [];
+  let i = 0;
 
   while (imposter) {
-    let whereSus = sus.indexOf(imposter) + 1;
-    console.log("When the imposter (" + imposter + ") is sus at character " + whereSus + ". AH! AMOGUS!");
-    if (isAMOGUSREFERENCEed) {
-      sussound.play();
-    }
+    let whereSus = imposter.index + 1;
+    susARRAY.push({imposter:imposter,whereSus:whereSus})
     imposter = find.exec(sus);
+    i++
+  }
+  if (isAMOGUSREFERENCEed) {
+    let f = 0;
+    let amogus = setInterval(function(){
+      if (f == i) {
+        clearInterval(amogus);
+        return
+      }
+      console.log("When the imposter (" + susARRAY[f].imposter + ") is sus at character " + susARRAY[f].whereSus + ". AH! AMOGUS!");
+      let sussound = new Sound('./amogus.wav');
+      sussound.play();
+      f++
+      if (f != i) {
+        setTimeout(function(){
+          sussound.pause();
+        }, 1000, sussound);
+      }
+    }, 500);
   }
 })
